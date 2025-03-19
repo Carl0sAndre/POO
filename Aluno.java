d3ed9a78fc3cd498a9172d80cd10e9f2310c409a
Carl0sAndre <160773664+Carl0sAndre@users.noreply.github.com>
2025-03-19 10:48:48 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.mavenproject1;
 import java.util.Random;
-/**
- *
- * @author alunolab11
- */
+import java.time.Year;
+
 public class Aluno {
+    private static int qtdalunos = 0;
     private String matricula;
     private String nome;
     private String curso;
@@ -17,8 +12,13 @@ public class Aluno {
     private double nota_1b;
     private double nota_2b;
     private double nota_final;
-    private static int qtdalunos = 0;
-   
+    
+    public Aluno(){
+        this.matricula = gerarMatricula(matricula);
+        this.nota_final = calcularFinal();
+        qtdalunos++;
+    }
+    
     public Aluno(String nome, String curso, String turma, int periodo, double nota_1b, double nota_2b) {
         this.matricula = gerarMatricula(matricula);
         this.nome = nome.toUpperCase();
@@ -33,14 +33,26 @@ public class Aluno {
     
     
     public static String gerarMatricula(String matricula){
-        int ano = 2025;
         Random random = new Random();
+        int ano = Year.now().getValue();
         int x = random.nextInt(10000);
         return String.format("%d%04d", ano, x);
     }
     
-    public double calcularFinal(){
-        return (this.nota_1b + this.nota_2b) / 2.0;
+    public double calcularFinal() {
+        return (this.nota_1b + this.nota_2b) / 2.0; 
+    }
+    
+    public boolean passar(String novaTurma, int novoPeriodo){
+        if (this.nota_final >= 7){
+            this.turma = novaTurma;
+            this.periodo = novoPeriodo;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     
     public void imprimir(){
@@ -49,11 +61,62 @@ public class Aluno {
         System.out.println("Curso: " + this.curso);
         System.out.println("Turma: " + this.turma);
         System.out.println("Período: " + this.periodo);
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        
-        
+        System.out.println("Nota primeiro bimestre: " + this.nota_1b);
+        System.out.println("Nota segundo bimestre: " + this.nota_2b);
+        System.out.println("Nota final: " + this.nota_final);
     }
+    
+    public void imprimirQtdDeAlunos(){
+        System.out.println("Quantidade total de alunos: " + qtdalunos);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public int getPeriodo() {
+        return periodo;
+    }
+
+    public double getNota_1b() {
+        return nota_1b;
+    }
+
+    public double getNota_2b() {
+        return nota_2b;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome.toUpperCase();
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public void setPeriodo(int periodo) {
+        this.periodo = periodo;
+    }
+
+    public void setNota_1b(double nota_1b) {
+        this.nota_1b = nota_1b;
+    }
+
+    public void setNota_2b(double nota_2b) {
+        this.nota_2b = nota_2b;
+    }
+    
     
 }
